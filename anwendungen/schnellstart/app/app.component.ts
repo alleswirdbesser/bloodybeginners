22ad56bcd4503309132a44b3d1fb1ad486e0ffd6
Andre Rauer <alleswirdbesser@web.de>
2020-01-11 23:52:26 +0100

    Schnellstart Anwendung zum Buch Angular (SystemJS)

---
+++ anwendungen/schnellstart/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h2>{{ title }}</h2>'
+})
+export class AppComponent {
+    title = 'Schnellstart Applikation mit Modulloader SystemJS läuft!' ;
+}
